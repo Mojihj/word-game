01e89ff895ca0937bce4ac8db751de5e2365c927
mojtaba.honarjoo <113408050+Mojihj@users.noreply.github.com>
2022-12-12 20:35:32 +0330

    Update word_game

--- word_game.py
+++ word_game.py
@@ -1,15 +1,47 @@
 import random
+HANGMAN_PICS = ['''
+ +---+
+     |
+     |
+     |
+    ===''', '''
+ +---+
+ O   |
+     |
+     |
+    ===''', '''
+ +---+
+ O   |
+ |   |
+     |
+    ===''', '''
+ +---+
+ O   |
+/|   |
+     |
+    ===''', '''
+ +---+
+ O   |
+/|\  |
+     |
+    ===''', '''
+ +---+
+ O   |
+/|\  |
+/    |
+    ===''', '''
+ +---+
+ O   |
+/|\  |
+/ \  |
+    ===''']
 s_word="ant camel cat cobra parrot pigeon rabbit salmon shark sheep".split()
 #function for choose a random word
-def choose_secret_word(wordlist):
-    word_index=random.randint(0,len(wordlist)-1)
-    return (wordlist[word_index])
 #Function for check if the input character by user is correct
 def check_guess_char(char):
     while True:
         print("Enter your guess:")
-        guess_char=input()
-        guess_char=guess_char.lower()
+        guess_char=input().lower()
         if len(guess_char) !=1:
             print("Enter only one character")
         elif guess_char not in "abcdefghijklmnopqrstuvwxyz":
@@ -20,31 +52,32 @@ def check_guess_char(char):
             return guess_char
 c_word=""
 m_word=""
-secret_word=choose_secret_word(s_word)
+secret_word=random.choice(s_word)
 gameIsDone = False
 blanks = '_' * len(secret_word)
 print("This is word guessin game".center(50,'-'))
-print("The secreet word is:",secret_word,"(for test)")
 while True:
+    print(HANGMAN_PICS[len(m_word)])
     guess1=check_guess_char(c_word+m_word)
     if guess1 in secret_word:
+        c_word=c_word+guess1
         for i in range(len(secret_word)):
             if secret_word[i] in guess1:
                 blanks=blanks[:i]+secret_word[i]+blanks[i+1:]
                 print("The secret word is:\n",blanks)
                 if "_" not in blanks:
                     print('Yes! The secret word is "' + secret_word + '"! You have won!')
-                    break
+                    gameIsDone=True          
     else:
         m_word=m_word+guess1
-        print("you have only",(len(secret_word)-len(m_word)),"chance left")
-        if len(m_word)==len(secret_word):
+        if len(m_word)==len(HANGMAN_PICS):
             print('You have run out of guesses')
             print("Do you want to play again?")
             x=input("Enter yes or no!!\n")
             if x=="yes":
                 m_word=""
-                agameIsDone = False
+                gameIsDone = False
+                print("This is word guessin game".center(50,'-'))
+                secret_word=random.choice(s_word)
             else:
-                gameIsDone = True
-                break
+                 break
